a55a268933577dc3ec110134f3a91dd75fbd4340
Diego <diego.almeida@sabion.com.br>
2022-08-17 15:52:55 -0300

    documentation

--- client-test/src/routers/routers.ts
+++ client-test/src/routers/routers.ts
@@ -8,33 +8,31 @@ itensRouter.post('/search', async (req, res) => {
     return res.json(response.data);
 });
 itensRouter.post('/aluguel', async (req, res) => {
-    console.log(req.body) 
-    const response =  await  axios.post("http://localhost:8080/api/aluguel", req.body);
-    return res.json(response.data);
+    try{
+        const response =  await  axios.post("http://localhost:8080/api/aluguel", req.body);
+        return res.json(response.data);
+    }catch(err:any){
+        if(err.response.status == 404){
+            return res.json({erro:"quantidade insuficiente"});
+        }
+        return res.json(err);
+    }
+    
 });
 itensRouter.put('/devolucao/:id', async (req, res) => {
     const id =  req.params.id;
-    const response =  await  axios.post("http://localhost:8080/api/aluguel/devolucao/"+id);
+    const response =  await  axios.put("http://localhost:8080/api/aluguel/devolucao/"+id);
     return res.json(response.data);
 });
 itensRouter.post('/venda', async (req, res) => {
-    const response =  await  axios.post("http://localhost:8080/api/venda", req.body);
-    return res.json(response.data);
+    try{
+        const response =  await  axios.post("http://localhost:8080/api/venda", req.body);
+        return res.json(response.data);
+    }catch(err:any){
+        if(err.response.status == 404){
+            return res.json({erro:"quantidade insuficiente"});
+        }
+        return res.json(err);
+    }
 });
-
-// itensRouter.get('/alugar', (req, res) => {
-//     res.send('Lê todos os itens')
-// })
-// itensRouter.get('/itens/:id', (req, res) => {
-//     const id: number = +req.params.id
-//     res.send(`Lê o item ${id}`)
-// })
-// itensRouter.put('/itens/:id', (req, res) => {
-//     const id: number = +req.params.id
-//     res.send(`Atualiza o item ${id}`)
-// })
-// itensRouter.delete('/itens/:id', (req, res) => {
-//     const id: number = +req.params.id
-//     res.send(`Apaga o item ${id}`)
-// })
 export default itensRouter
